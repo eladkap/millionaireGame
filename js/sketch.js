85b116fd2b9f64f9e50193b5ed8d6522c9d53c40
eladkapuza <eladkapuza@gmail.com>
2020-08-23 17:01:03 +0300

    added logs for loading sound files

--- js/sketch.js
+++ js/sketch.js
@@ -236,32 +236,50 @@ function StartClock() {
 
 /* Load Sound */
 function LoadSoundFiles() {
-  rulesSong = loadSound(RULES_SONG);
-  letsPlaySong = loadSound(LETS_PLAY_SONG);
-  easyQuestionsSong = loadSound(EASY_QUESTIONS_SONG);
-  mediumQuestionsSong = loadSound(MEDIUM_QUESTIONS_SONG);
-  hardQuestionsSong = loadSound(HARD_QUESTIONS_SONG);
-  cut5050Sound = loadSound(CUT_5050_SOUND);
-  easyRightAnswerSound = loadSound(EASY_RIGHT_ANSWER_SOUND);
-  mediumRightAnswerSound = loadSound(MEDIUM_RIGHT_ANSWER_SOUND);
-  hardRightAnswerSound = loadSound(HARD_RIGHT_ANSWER_SOUND);
-  loseSound = loadSound(LOSE_SOUND);
-  finalMediumAnswerSound = loadSound(FINAL_MEDIUM_ANSWER_SOUND);
-  finalHardAnswerSound = loadSound(FINAL_HARD_ANSWER_SOUND);
-  phoneClockSound = loadSound(PHONE_CLOCK_SOUND);
+  console.log("Loading sound files...");
+  try {
+    rulesSong = loadSound(RULES_SONG);
+    console.log("Sound1");
+    letsPlaySong = loadSound(LETS_PLAY_SONG);
+    console.log("Sound2");
+    easyQuestionsSong = loadSound(EASY_QUESTIONS_SONG);
+    console.log("Sound3");
+    mediumQuestionsSong = loadSound(MEDIUM_QUESTIONS_SONG);
+    console.log("Sound4");
+    hardQuestionsSong = loadSound(HARD_QUESTIONS_SONG);
+    console.log("Sound5");
+    cut5050Sound = loadSound(CUT_5050_SOUND);
+    console.log("Sound6");
+    easyRightAnswerSound = loadSound(EASY_RIGHT_ANSWER_SOUND);
+    console.log("Sound7");
+    mediumRightAnswerSound = loadSound(MEDIUM_RIGHT_ANSWER_SOUND);
+    console.log("Sound8");
+    hardRightAnswerSound = loadSound(HARD_RIGHT_ANSWER_SOUND);
+    console.log("Sound9");
+    loseSound = loadSound(LOSE_SOUND);
+    console.log("Sound10");
+    finalMediumAnswerSound = loadSound(FINAL_MEDIUM_ANSWER_SOUND);
+    console.log("Sound11");
+    finalHardAnswerSound = loadSound(FINAL_HARD_ANSWER_SOUND);
+    console.log("Sound12");
+    phoneClockSound = loadSound(PHONE_CLOCK_SOUND);
+    console.log("Sound13");
 
-  rulesSong.setVolume(0.5);
-  letsPlaySong.setVolume(0.5);
-  easyQuestionsSong.setVolume(0.5);
-  mediumQuestionsSong.setVolume(0.5);
-  hardQuestionsSong.setVolume(0.5);
-  cut5050Sound.setVolume(0.5);
-  easyRightAnswerSound.setVolume(0.5);
-  mediumRightAnswerSound.setVolume(0.5);
-  hardRightAnswerSound.setVolume(0.5);
-  finalMediumAnswerSound.setVolume(0.5);
-  finalHardAnswerSound.setVolume(0.5);
-  loseSound.setVolume(0.5);
+    rulesSong.setVolume(0.5);
+    letsPlaySong.setVolume(0.5);
+    easyQuestionsSong.setVolume(0.5);
+    mediumQuestionsSong.setVolume(0.5);
+    hardQuestionsSong.setVolume(0.5);
+    cut5050Sound.setVolume(0.5);
+    easyRightAnswerSound.setVolume(0.5);
+    mediumRightAnswerSound.setVolume(0.5);
+    hardRightAnswerSound.setVolume(0.5);
+    finalMediumAnswerSound.setVolume(0.5);
+    finalHardAnswerSound.setVolume(0.5);
+    loseSound.setVolume(0.5);
+  } catch (error) {
+    console.log(error);
+  }
 
   questionsSongs = [easyQuestionsSong, mediumQuestionsSong, hardQuestionsSong];
 }
