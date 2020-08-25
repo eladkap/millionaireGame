436a1d8aebc87c2377fb3de419bcfba06482e032
eladkapuza <eladkapuza@gmail.com>
2020-08-25 23:22:49 +0300

    added question and answers areas

--- js/game.js
+++ js/game.js
@@ -1,16 +1,26 @@
-async function LoadGame() {
+function LoadGame() {
   console.log("Load game");
   LoadSoundFiles();
   document.getElementById("img_cover").style.opacity = 0.1;
-  SOUNDS_DICT["start_game"].SetCallback(LoadFirstQuestion);
-  SOUNDS_DICT["start_game"].Play();
-  // await Sleep(SOUNDS_DICT["start_game"].GetDuration() * 1000);
+  // SOUNDS_DICT["start_game"].SetCallback(LoadFirstQuestion); todo: uncomment
+  // SOUNDS_DICT["start_game"].Play(); todo: uncomment
+  LoadFirstQuestion(); // todo: comment
 }
 
 function LoadFirstQuestion() {
   console.log("Load first question");
-  questionText =
+  let questionText =
     "A flashing red traffic light signifies that a driver should do what?";
-  document.getElementById("question_area").value = questionText;
+  let answerA = "Answer A";
+  let answerB = "Answer B";
+  let answerC = "Answer C";
+  let answerD = "Answer D";
+
+  document.getElementById("question_area").innerHTML = questionText;
+  document.getElementById("answerA_area").innerHTML = "<h2>♦A: answerA </h2>";
+  document.getElementById("answerB_area").innerHTML = "<h2>♦B: answerB </h2>";
+  document.getElementById("answerC_area").innerHTML = "<h2>♦C: answerC </h2>";
+  document.getElementById("answerD_area").innerHTML = "<h2>♦D: answerD </h2>";
+
   console.log("Question loaded");
 }
